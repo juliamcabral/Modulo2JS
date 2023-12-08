ad82eaea10b68f987995777fcfba3e156a0f6186
juliamcabral <jumoreiraac@gmail.com>
2023-12-08 09:38:09 -0300

    Desafio 1

---
+++ script.js
@@ -0,0 +1,47 @@
+let cantora = "Mariah Carey"
+console.log(cantora)
+if(cantora =="Mariah Carey"){
+    console.log ("Ano novo está chegando!!")
+}
+
+let idadeMariahCarey = 54
+console.log(idadeMariahCarey)
+
+let anoatual = 2023
+console.log(anoatual = anoatual -= 54 )
+
+let quartaFeira = "true"
+console.log(typeof quartaFeira)
+console.log(quartaFeira)
+
+let idade = 18
+
+if(idade >= 18){
+    console.log("Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey")
+} else (idade < 18)
+    console.log("Sinto muito, mas assistirá da tv globo")
+
+
+let aniversario = "Junho"
+let Aniversario = "Agosto"
+let _aniversario = "Dezembro"
+
+if (aniversario == "Junho"){
+console.log("Uma ou mais opções estão corretas. O mês escolhido foi o mês de Junho")
+}
+if (Aniversario == "Agosto"){
+    console.log("Uma ou mais opções estão corretas. O mês escolhido foi o mês de Agosto")
+}if (_aniversario == "Dezembro"){
+    console.log("Uma ou mais opções estão corretas. O mês escolhido foi o mês de Dezembro")
+} 
+
+else (aniversario == "Setembro" || Aniversario =="Março" || _aniversario == "Abril" )
+    console.log("Algo de errado não está certo, o mês digitado foi o mês de Setembro.")
+    console.log("Algo de errado não está certo, o mês digitado foi o mês de Março.")
+    console.log("Algo de errado não está certo, o mês digitado foi o mês de Abril.")
+    
+
+    
+
+
+
